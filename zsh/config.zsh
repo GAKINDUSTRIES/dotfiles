d3fc35c3bd4c0919d245edb4f1d01f0cc6355da3
Guillermo Kuster <guillermo@techsniffer.me>
2019-10-08 09:33:54 -0300

    Add syntax highlighting

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -7,10 +7,13 @@ DISABLE_AUTO_TITLE="true"
 
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(sublime bundler npm rails zsh-syntax-highlighting history web-search vi-mode)
+plugins=(sublime bundler npm rails history web-search vi-mode)
 
 # Path to oh-my-zsh installation.
 export ZSH=$HOME/.oh-my-zsh
 
 # Load oh-my-zsh
 source $ZSH/oh-my-zsh.sh
+
+# Load syntax highlighting
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
