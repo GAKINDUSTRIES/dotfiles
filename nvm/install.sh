1406529439b3a1b6a8495ce8580eb7a00800100c
Guillermo Kuster <guillermo@techsniffer.me>
2019-10-21 09:51:07 -0300

    Add missing "$" to nvm/install.sh env variable

--- nvm/install.sh
+++ nvm/install.sh
@@ -6,7 +6,7 @@
 \curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
 
 NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Install Node LTS version
 nvm install --lts
