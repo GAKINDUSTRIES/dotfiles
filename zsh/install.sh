5169380e9cec51bbf4bc8c8f171960ba8b791093
Guillermo Kuster <guillermo@techsniffer.me>
2019-10-26 09:51:07 -0300

    Check if oh-my-zsh is already on the machine before installing it

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,4 +8,9 @@ chsh -s /bin/zsh # Switch shell to zsh
 # Install oh-my-zsh plugin.
 # --unattended flag will have the efect of not trying to change the defaul shell,
 # and also won't run zsh when the installation has finished.
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+if [ -d "$HOME/.oh-my-zsh" ] 
+then
+  echo "oh-my-zsh already exists. Skipping..." 
+else
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+fi
