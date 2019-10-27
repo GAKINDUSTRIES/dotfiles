1122d501ae714ea872c506b83fa10b4c07afed04
Guillermo Kuster <guillermo@techsniffer.me>
2019-10-27 09:51:07 -0300

    Modify ruby script and install latest ruby version after installing RVM

--- ruby/install.sh
+++ ruby/install.sh
@@ -3,4 +3,14 @@
 # Script to install RVM
 # TODO, install frequently used rubies
 
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+# Install gpg keys
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
+\curl -sSL https://get.rvm.io | bash
+
+# Reload RVM config
+source ~/.rvm/scripts/rvm
+
+RUBY_VERSION=2.6.3
+
+rvm install "$RUBY_VERSION" --with-openssl-dir=`brew --prefix openssl`
