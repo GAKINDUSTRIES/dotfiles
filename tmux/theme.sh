f91bfc84dd2534672b25a34906daa66deae43f1f
Guillermo Kuster <guillermo.kuster@fullscript.com>
2021-02-25 11:28:51 -0500

    - ch script added  - gstash script added  - tunes.scpt re-added (itunes updated to the new Apple music)  - sc script modified  - tmux colors adjusted

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -7,10 +7,13 @@ tm_icon="♟"
 tm_color_active=colour51
 tm_color_inactive=colour241
 tm_color_feature=colour198
-tm_color_music=colour41
+tm_color_music=colour195
 tm_active_border_color=colour51
 tm_color_battery=colour196
 tm_color_battery_status=colour226
+tm_color_low_state=colour47
+tm_color_medium_state=colour227
+tm_color_high_state=colour1
 
 # separators
 tm_separator_left_bold="◀"
@@ -52,7 +55,7 @@ set-option -g display-panes-colour $tm_color_inactive
 set-window-option -g clock-mode-colour $tm_color_active
 
 # Display current track being played
-tm_spotify="#[fg=$tm_color_background,bg=$tm_color_music]#(osascript ~/dotfiles/tmux/applescripts/spotify.scpt)"
+tm_spotify="#[fg=$tm_color_music]#(osascript ~/dotfiles/tmux/applescripts/spotify.scpt)"
 tm_tunes="#[fg=$tm_color_music]#(osascript ~/dotfiles/tmux/applescripts/tunes.scpt)"
 
 # Display battery level
@@ -64,9 +67,9 @@ tm_host="#[fg=$tm_color_feature,bold]GAKINDUSTRIES"
 tm_session_name="#[fg=$tm_color_feature]$tm_icon #S"
 
 # Display cpu and ram stats
-set -g @ram_low_fg_color    "#[fg=colour47]"
-set -g @ram_medium_fg_color "#[fg=colour227]"
-set -g @ram_high_fg_color   "#[fg=colour1]"
+set -g @ram_low_fg_color    "#[fg=$tm_color_low_state]"
+set -g @ram_medium_fg_color "#[fg=$tm_color_medium_state]"
+set -g @ram_high_fg_color   "#[fg=$tm_color_high_state]"
 set -g @ram_medium_thresh   "30"    # medium percentage threshold
 set -g @ram_high_thresh     "85"    # high percentage threshold
 
@@ -75,5 +78,5 @@ tm_mem_ram="#{ram_fg_color} 🧠 #{ram_percentage}"
 
 # Set status bar
 set -g status-left $tm_session_name'      '
-set -g status-right $tm_spotify'        '$tm_mem_ram'   '$tm_mem_cpu'   '$tm_battery'   '$tm_date'   '$tm_host''
+set -g status-right $tm_tunes'       '$tm_mem_ram'   '$tm_mem_cpu'   '$tm_battery'   '$tm_date'   '$tm_host''
 
